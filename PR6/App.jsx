0873f52af6fcf2f65455c05b6a4b41805961a850
Tanmay Shinde <ts8161624@gmail.com>
2025-10-15 19:41:27 +0530

    Create App.jsx
    Create a React application using JSX with functional components. Pass data using props and manage state using the useState hook.

---
+++ PR6/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import DisplayUsers from './DisplayUsers';
+import Counter from './Counter';
+import Form1 from './Form1';
+
+
+function App() {
+  
+  return (
+    <div>
+      
+      <h1>Form Example with useState</h1>
+      <Form1 />
+    </div>
+  );
+}
+
+export default App;
